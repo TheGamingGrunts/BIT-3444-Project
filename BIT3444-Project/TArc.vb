d05e917a9b2864007986baece58fb488ccf3ef2c
giantsfan <thegurugamers@gmail.com>
2017-04-10 11:48:00 -0400

    Add optimization class

---
+++ BIT3444-Project/TArc.vb
@@ -0,0 +1,19 @@
+﻿Public Class TArc
+
+    Inherits Arc
+
+    Public Property PathList As New List(Of String())
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(t As Node, h As Node)
+        MyBase.New(t, h, 0, 0)
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return MyBase.ToString & ", Trans Paths: " & PathList.Count
+    End Function
+
+End Class
